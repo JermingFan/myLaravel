34d5999909b07764b612b76d895b923f65f8825c
JermingFan <jermingfan@foxmail.com>
2016-04-01 14:59:47 +0800

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,43 +2,49 @@
     <div class="header">
         <div class="container">
             <span class="menu">Menu</span>
-
-            <div class="banner-top">
-                <ul class="nav banner-nav">
-                    <li><a class="" href="{{ URL('/') }}">主页</a></li>
-                    <li><a href="{{ URL('/project') }}">xxx</a></li>
-                    <li><a href="{{ URL('/talent') }}">xxx</a></li>
-                    <li><a href="{{ URL('/partner') }}">xxx</a></li>
-
-                    @if (Auth::guest())
-                        <li class="navbar-right"><a href="{{ URL('/login') }}" type="button" class="dt-sc-button small">登录</a></li>
-                        <li class="navbar-right"><a href="{{ URL('/register') }}" type="button" class="dt-sc-button small">注册</a></li>
-                    @else
-                        <li class="dropdown1 navbar-right">
-                            <a href="#" class="" data-toggle="dropdown">
-                                {{ Auth::user()->name }}
-                            </a>
-                            <ul class="dropdown2" role="menu">
-                                <li><a href="{{ url('/userinfo') }}">个人资料</a></li>
-                                <li><a href="{{ url('/logout') }}">注销</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                    <form>
-                        <div class="nav search-bar">
-                            <input type="text"  value="Search" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Search';}">
-                            <input type="submit" value="" />
-                        </div>
-                    </form>
-                </ul>
-
-                <script>
-                    $("span.menu").click(function(){
-                        $(" ul.nav").slideToggle("slow" , function(){
-                        });
-                    });
-                </script>
-            </div>
+            <ul class="nav banner-nav">
+                <li><a href="{{ url('/') }}">主页</a></li>
+                <li><a href="{{ url('/project') }}">项目</a></li>
+                <li><a href="{{ url('/partner') }}">识才</a></li>
+                @if (Auth::guest())
+                    <li class="navbar-right"><a href="{{ url('/login') }}" type="button" class="">登录</a></li>
+                    <li class="navbar-right"><a class="register-link" href="{{ url('/register') }}" type="button" >注册</a></li>
+                @else
+                    <li class="dropdown1 navbar-right">
+                        <a href="javascript:;" data-toggle="dropdown">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown2" role="menu">
+                            <li><a href="{{ url('/setting') }}">基本资料</a></li>
+                            <li><a href="{{ url('/logout') }}">注销</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown1 navbar-right">
+                        <a href="javascript:;" data-toggle="dropdown">
+                            发布
+                        </a>
+                        <ul class="dropdown2" role="menu">
+                            <li><a href="{{ url('/myProject') }}">项目</a></li>
+                            <li><a href="{{ url('/profile') }}">简历</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown1 navbar-right">
+                        <a href="javascript:;" data-toggle="dropdown">
+                            关注
+                        </a>
+                        <ul class="dropdown2" role="menu">
+                            <li><a href="{{ url('/projectFocus') }}">关注的项目</a></li>
+                            <li><a href="{{ url('/partnerFocus') }}">关注的人才</a></li>
+                        </ul>
+                    </li>
+                    <li class="navbar-right">
+                        <a href="{{ url('/toNews') }}">消息<i id="news" style="color: rgb(70,160,230); font-size: 1px;"></i>
+                        </a>
+                    </li>
+                @endif
+            </ul>
         </div>
     </div>
 </nav>
+@section('scripts')
+@endsection
