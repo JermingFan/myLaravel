5a0c0b65d2610ab5c7ea4da7136585782c8de08e
JermingFan <jermingfan@foxmail.com>
2016-04-08 22:20:33 +0800

    update

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interest_relations;
+use App\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
+
+class MessageController extends Controller
+{
+    protected $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+    public function getMessage(Request $request)
+    {
+        if (Auth::check())
+        {
+            $mes = $this->message->getMes($request->user()->id);
+            if(!empty($mes->id))
+            {
+                return $this->_returnMessageFile('您有一条新消息',200);
+            }
+            return $this->_returnMessageFile('no news',100);
+        }
+        else return redirect()->guest('login');
+    }
+    public function toMessage()
+    {
+//        if (Auth::check())
+//        {
+//            $mesFocus = $this->message->getMesFocus($request->user()->id);
+            $mesInterest = $this->message->getMesInterest(3);
+        var_dump($mesInterest);
+//        var_dump($mesInterest);die;
+//            $data['focus'] = $mesFocus;
+//            $data['interest'] = $mesInterest;
+//            //设置为已读
+//            $data['signal'] = 1;
+//            $this->message->signMes((int)$request->user()->id);
+//            return view('users.news')->with('data',$data);
+//        }
+//        else return redirect()->guest('login');
+    }
+    //同意
+    public function toAgree(Request $request)
+    {
+        if (Auth::check())
+        {
+            $all = $request->all();
+            $interest = new Interest_relations();
+            $interest->updateInterest($all['from_id'],$request->user()->id,$all['type']);
+            return $this->_returnMessage('success',200,$all['signal']);
+        }
+        else return redirect()->guest('login');
+    }
+}
