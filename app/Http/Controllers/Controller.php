f5942f20032c5ed225adf9a5f27e71ee80ca4c4e
JermingFan <jermingfan@foxmail.com>
2016-04-09 22:45:00 +0800

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,12 +14,13 @@ class Controller extends BaseController
     {
         $this->middleware('auth');
     }
-    protected function _returnMessage($content,$state)
+    protected function _returnMessage($content,$state,$data='')
     {
         return response()->json(
             array(
                 'content'=>$content,
                 'state'=>$state,
+                'signal'=>$data
             )
         );
     }
@@ -28,5 +29,19 @@ class Controller extends BaseController
         $result['state'] = $state;
         $result['content'] = $content;
         return json_encode($result);
+    }
+    protected function hide($keyword)
+    {
+        if (strstr($keyword, '@'))
+        {
+            $start = strpos($keyword, '@');
+            $length = strlen($keyword);
+            $keyword = substr($keyword, 0, 2) . '****' . substr($keyword, $start, $length - $start);
+        }
+        else
+        {
+            $keyword = substr($keyword, 0, 3) . '****' . substr($keyword, strlen($keyword) - 4, 4);
+        }
+        return $keyword;
     }
 }
---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Profile;
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+use zgldh\QiniuStorage\QiniuStorage;
+
+class ProfileController extends Controller
+{
+    protected $profile;
+    public function __construct(Profile $profile)
+    {
+        parent::__construct();
+        $this->profile = $profile;
+    }
+    //展示个人资料
+    public function show(Request $request)
+    {
+        if (Auth::check())
+        {
+            $user_info = $this->profile->getInfo($request->user()->id);
+            if(!empty($user_info))
+            {
+                if(empty($user_info->profile_img))$user_info->profile_img = PROFILE_IMG;//默认项目图片
+                $user_info->age = isset($user_info->age)?unserialize(AGE)[$user_info->age]:'';
+                $user_info->place = isset($user_info->place)?unserialize(PLACE)[$user_info->place]:'';
+                $user_info->experience = isset($user_info->experience)?unserialize(EXPERIENCE)[$user_info->experience]:'';
+//            $user_info->email = $this->hide($user_info->email);
+//            $user_info->phone = $this->hide($user_info->phone);
+            }
+            return view('users.profile')->with('user_info',$user_info);
+        }
+        else return redirect()->guest('login');
+    }
+    //编辑
+    public function edit(Request $request)
+    {
+        if (Auth::check())
+        {
+            $user_info = $this->profile->getInfo($request->user()->id);
+            if(empty($user_info->profile_img))$user_info->profile_img = PROFILE_IMG;
+            //邮箱默认为注册邮箱
+            if(empty($user_info->email))
+            {
+                $user = new User();
+                $user_info->email = $user->getEmail($request->user()->id)->email;
+            }
+            $user_info->ages = unserialize(AGE);
+            $user_info->places = unserialize(PLACE);
+            $user_info->experiences = unserialize(EXPERIENCE);
+            return view('users.profileEdit')->with('user_info',$user_info);
+        }
+        else return view('auth.login');
+    }
+    //编辑上传
+    public function update(Request $request)
+    {
+        if (Auth::check())
+        {
+            $all = $request->all();
+            //简历图片
+            $disk = QiniuStorage::disk('qiniu');
+            $all['url'] = '';
+            if(!empty($_FILES["img"]["tmp_name"]))
+            {
+                if($disk->put( date('Y/m/d').'/'.md5($_FILES["img"]["name"]), file_get_contents($_FILES["img"]["tmp_name"])))
+                {
+                    $all['url'] = $disk->downloadUrl(date('Y/m/d').'/'.md5($_FILES["img"]["name"]));
+                }
+            }
+            //查询是否创建过
+            $user_info = $this->profile->getInfo($request->user()->id);
+            if(empty($user_info))
+            {
+                $insert = $this->profile->insertInfo($request->user()->id,$all);
+                if($insert !=1 )
+                {
+                    return $this->_returnMessageFile('插入失败',101);
+                }
+            }
+            else
+            {
+                $update = $this->profile->updateInfo($request->user()->id,$all);
+                if($update !=1 )
+                {
+                    return $this->_returnMessageFile('更新失败',101);
+                }
+            }
+
+            return $this->_returnMessageFile('修改成功',200);
+        }
+        else return redirect()->guest('login');
+    }
+}
