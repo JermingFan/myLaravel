92f25071d6ebaa32c051dede89a11900285e1c3b
JermingFan <jermingfan@foxmail.com>
2016-04-26 23:03:37 +0800

    style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,58 +2,41 @@
 @section('styles')
 @endsection
 @section('content')
-    <div class="wrapper">
-        <div class="container">
-            <div class="col-sm-5 col-sm-offset-1 setting">
+    <div class="wrapper" id="register-bg">
+        <div class="container" style="padding-top: 0;padding-bottom: 0;">
+            <div class="setting">
+            <div class="col-sm-2 col-sm-offset-3">
+                @if (count($errors) > 0)
+                    <div class="">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+            <div class="col-sm-5 col-sm-offset-1">
                 <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                     {!! csrf_field() !!}
-                    @if (count($errors) > 0)
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
                     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                         <div style="font-size: 1.5em">姓名<label>*</label></div>
                         <input type="text" name="name" value="{{ old('name') }}" class="setting-input-text">
-                        @if ($errors->has('name'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                        @endif
                     </div>
 
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                         <div style="font-size: 1.5em">邮箱<label>*</label></div>
                         <input type="email" name="email" class="setting-input-text" value="{{ old('email') }}">
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
                     </div>
 
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                         <div style="font-size: 1.5em">密码<label>*</label></div>
                         <input type="password" name="password" class="setting-input-text">
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
                     </div>
 
                     <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                         <div style="font-size: 1.5em">确认密码<label>*</label></div>
                         <input type="password" name="password_confirmation" class="setting-input-text">
-                        @if ($errors->has('password_confirmation'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password_confirmation') }}</strong>
-                            </span>
-                        @endif
                     </div>
                     <div class="form-group{{ $errors->has('captcha') ? ' has-error' : '' }}">
                         <div style="font-size: 1.5em">拖动验证<label>*</label></div>
@@ -85,10 +68,11 @@
                     </div>
                     <div class="clearfix"></div>
                     <div>
-                    <a class="forgot" href="{{ url('/login') }}">已有帐号？</a>
+                        <a class="forgot" href="{{ url('/login') }}">已有帐号？</a>
                         <input type="submit" class="setting-submit" value="注册" >
                     </div>
                 </form>
+            </div>
             </div>
         </div>
     </div>
